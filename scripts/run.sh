15bdb2f6aade5f00b7d8b65e309fa423e033377e
gugmelik <gugmelik@gmail.com>
2022-04-29 02:34:47 +0300

    added script create virtual enviroment and to run detector

---
+++ scripts/run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+env_name="venv"
+
+if [ ! -d $env_name ]
+then
+  echo "Have not found existing $env_name, creating a new one"
+  pip install virtualenv
+  virtualenv --python=python3.7 $env_name
+  . ./$env_name/bin/activate && echo "successfully activated $env_name python is $(which python)"
+  pip install -r requirements.txt
+  cd models/research
+  # Compile protos.
+  apt install -y protobuf-compiler
+  protoc object_detection/protos/*.proto --python_out=.
+  # Install TensorFlow Object Detection API.
+  cp object_detection/packages/tf2/setup.py .
+  python -m pip install --use-feature=2020-resolver .
+  
+fi
+
+
+source $env_name/bin/activate
+python src/detect.py data/video.mp4
